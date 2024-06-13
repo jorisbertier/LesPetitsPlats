19e783b0a2e65132edd2fbebc6d460e5808a2335
Joris Bertier <joris.bertier@gmail.com>
2024-06-13 14:37:22 -0500

    gat data recipes fetch

---
+++ src/scripts/api/api.js
@@ -0,0 +1,20 @@
+async function getRecipes() {
+
+    let recipes = [];
+    try {
+        const response = await fetch("/data/recipes.js");
+        let data = await response.json();
+    
+        data.forEach(recipe => {
+            recipes.push(recipe)
+        });
+    } catch(error) {
+        console.log('Error getting datas recipes', error)
+    }
+    console.log(recipes)
+    return ({
+        recipes: [...recipes]
+    })
+}
+
+getRecipes()
