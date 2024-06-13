56dddb5a0671e2207a799c3b75d8438c89ddc62c
Joris Bertier <joris.bertier@gmail.com>
2024-06-13 15:29:54 -0500

    add a

--- src/scripts/templates/RecipeCard.js
+++ src/scripts/templates/RecipeCard.js
@@ -22,28 +22,30 @@ class RecipeCard {
         const formattedTime = formatTime(time)
 
         const content = `
-        <article class="bg-white rounded-3xl shadow-lg mb-5 w-96 h-650">
-                <div class="relative">
-                    <img class="w-full rounded-t-3xl h-64 object-cover"src="/src/img/lampos-aritonang-24gR_9lCdes-unsplash.jpg">
-                    <div class="absolute top-4 right-4 bg-primary-color w-20 p-2 rounded-xl text-center">${formattedTime}</div>
-                </div>
-                <div class="p-6 mt-3">
-                    <h3 class="text-black font-anton uppercase text-base mb-6">${name}</h3>
-                    <h4 class="text-secondary-color font-anton uppercase text-xs mb-2 font-thin tracking-widest">Recette</h4>
-                    <p class="text-sm font-manrope">${description}.</p>
-                    <h4 class="text-secondary-color font-anton uppercase text-xs font-thin tracking-widest mt-6 mb-3">Ingrédients</h4>
-                    <div class="flex justify-between w-4/5">
-                        <div>
-                            <h5 class="text-sm text-black">Lait de Coco</h5>
-                            <p class="text-secondary-color uppercase text-xs">400ml</p>
-                        </div>
-                        <div>
-                            <h5 class="text-sm text-black">Lait de Coco</h5>
-                            <p class="text-secondary-color uppercase text-xs">400ml</p>
+        <a href="#">
+            <article class="bg-white rounded-3xl shadow-lg mb-5 w-96 h-650">
+                    <div class="relative">
+                        <img class="w-full rounded-t-3xl h-64 object-cover"src="/src/img/lampos-aritonang-24gR_9lCdes-unsplash.jpg">
+                        <div class="absolute top-4 right-4 bg-primary-color w-20 p-2 rounded-xl text-center">${formattedTime}</div>
+                    </div>
+                    <div class="p-6 mt-3">
+                        <h3 class="text-black font-anton uppercase text-base mb-6">${name}</h3>
+                        <h4 class="text-secondary-color font-anton uppercase text-xs mb-2 font-thin tracking-widest">Recette</h4>
+                        <p class="text-sm font-manrope">${description}.</p>
+                        <h4 class="text-secondary-color font-anton uppercase text-xs font-thin tracking-widest mt-6 mb-3">Ingrédients</h4>
+                        <div class="flex justify-between w-4/5">
+                            <div>
+                                <h5 class="text-sm text-black">Lait de Coco</h5>
+                                <p class="text-secondary-color uppercase text-xs">400ml</p>
+                            </div>
+                            <div>
+                                <h5 class="text-sm text-black">Lait de Coco</h5>
+                                <p class="text-secondary-color uppercase text-xs">400ml</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </article>
+                </article>
+            </a>
         `
         template.innerHTML += content
         return content
