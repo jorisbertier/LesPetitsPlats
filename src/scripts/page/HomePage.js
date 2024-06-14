a36934207aaa88a4b29df348b1b095f9a28fd772
Joris Bertier <joris.bertier@gmail.com>
2024-06-14 12:01:04 -0500

    add function delete and style

--- src/scripts/page/HomePage.js
+++ src/scripts/page/HomePage.js
@@ -3,24 +3,12 @@ import { getRecipes } from "../api/api.js";
 
 let inputSearch = document.getElementById('recipe')
 let search = document.querySelector('.search')
+let removeSearch = document.querySelector('.remove__result')
 
-// function searchByTitle(recipes, query) {
-//     query = query.toLowerCase()
-//     console.log(recipes.filter(recipe => recipe.name.toLowerCase().includes(query)))
-//     return recipes.filter(recipe => recipe.name.toLowerCase().includes(query))
-// }  
-
-
-// console.log(search)
-//     search.addEventListener('click', ()=> {
-//         console.log(inputSearch.value)
-//         if(recipe.name === recipe.name.include(inputSearch.value)) {
-//             return recipe
-//         }
-//     })
-
-//au click je lance la recherhce
-// si la recherche correspond au mot j'affiche que les elements qui corresponde a ce mot
+removeSearch.addEventListener('click', ()=> {
+    inputSearch.value = ""
+    console.log('delete')
+})
 
 async function displayRecipes() {
     const { recipes } = await getRecipes()
@@ -35,13 +23,11 @@ async function displayRecipes() {
 
 }
 
+
+
 function renderRecipes(recipes) {
     let wrapperRecipes = document.querySelector('.wrapper__recipes')
 
-    // while (wrapperRecipes.firstChild) {
-    //     wrapperRecipes.removeChild(wrapperRecipes.firstChild);
-    // }
-    
     wrapperRecipes.innerHTML = ""
 
     recipes.forEach((recipe) => {
