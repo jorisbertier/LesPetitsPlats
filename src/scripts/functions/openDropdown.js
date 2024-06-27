f968c3ae764dcde792deded1b03c21b3de5d2d29
Joris Bertier <joris.bertier@gmail.com>
2024-06-27 16:24:25 -0500

    add style

--- src/scripts/functions/openDropdown.js
+++ src/scripts/functions/openDropdown.js
@@ -12,22 +12,30 @@ let openDropdownAppliances = document.querySelector('.chevron-down--appliances')
 let closeDropdownAppliances = document.querySelector('.chevron-up--appliances')
 let wrapperAppliances = document.querySelector('.appliances')
 
+let buttonIngredient = document.querySelector('.button__ingredient')
+let buttonUstensils = document.querySelector('.button__ustensil')
+let buttonAppliances = document.querySelector('.button__appliance')
 
-function openAndCloseDropdown(open, close, wrapper) {
+function openAndCloseDropdown(open, close, wrapper, button) {
         open.addEventListener('click', ()=> {
         open.style.display = "none";
         close.style.display = "flex";
         wrapper.style.display = "flex"
+        button.style.borderBottomLeftRadius = "none"
+        button.classList.remove("rounded-md");
+        button.classList.add('rounded-t-md')
     })
     
     close.addEventListener('click', ()=> {
         open.style.display = "flex";
         close.style.display = "none";
         wrapper.style.display = "none"
+        button.classList.add("rounded-md");
+        button.classList.remove("rounded-t-md");
     })
     
 }
 
-openAndCloseDropdown(openDropdownIngredients, closeDropdownIngredients, wrapperIngredients)
-openAndCloseDropdown(openDropdownUstensils, closeDropdownUstensils, wrapperUstensils)
-openAndCloseDropdown(openDropdownAppliances, closeDropdownAppliances, wrapperAppliances)
+openAndCloseDropdown(openDropdownIngredients, closeDropdownIngredients, wrapperIngredients, buttonIngredient)
+openAndCloseDropdown(openDropdownUstensils, closeDropdownUstensils, wrapperUstensils, buttonUstensils)
+openAndCloseDropdown(openDropdownAppliances, closeDropdownAppliances, wrapperAppliances, buttonAppliances)
